5351b8bf8c96a041e15926475328f8d9618a9075
kiyoshi Nishikawa <kiyoshi.nishikawa@gmail.com>
2020-07-27 20:35:20 +0900

    Modified the css for index.html

--- index.js
+++ index.js
@@ -6,15 +6,28 @@
 // const log = require('electron-log')
 'use strict'
 
+function putTime(){
+    var now = new Date()
+    var year = now.getFullYear()
+    var month = now.getMonth + 1
+    const p = document.getElementById('infortime')
+    p.innerHTML = now
+}
+
 function timeset(value){
     const p = document.getElementById('timep')
     p.innerHTML = value
+
+    var now = new Date().getTime()
+    const q = document.getElementById('infortime')
+    q.innerHTML = now
 }
 
 const button = document.getElementById('qb')
 button.addEventListener('click', function(clickEvent) {
     const p = document.getElementById('timep')
     p.innerHTML = 'Quit'
+    window.close()
 })
 
 const bt = document.getElementsByClassName('btime')
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow(){
     })
 
     mainWindow.loadFile('index.html')
-//    mainWindow.webContents.openDevTools()
+    //mainWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(()=>{
